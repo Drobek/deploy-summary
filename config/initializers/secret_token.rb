0a84df02b1576c5f80ca6b519809fb5f07db0231
ondrej01 <ondrej.maly128@gmail.com>
2013-04-23 20:45:18 +0200

    + rails sceleton (will not be used at final version) + scripts/deploy.rb empty script file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClarityfmDeploySummary::Application.config.secret_token = '9e42d613634b4e40c4cf3b46e7143db8c6fa66d7bca0cf33322dee278a878986e08c10d9737f9bfead4e28bd5e1a850e7e0d7d926a41be5f7f94bb61085d825f'
