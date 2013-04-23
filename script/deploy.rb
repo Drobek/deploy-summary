5a1a705c09bff51e9f5e2afc0f2ab6a68e2c2be1
ondrej01 <ondrej.maly128@gmail.com>
2013-04-23 22:22:02 +0200

    + Commit class for later purposes

--- script/deploy.rb
+++ script/deploy.rb
@@ -1,3 +1,33 @@
 #!/usr/bin/env ruby
 
-puts "Hello World"
+require 'rubygems'
+require 'json'
+
+class Commit
+	@author_name
+	@author_email
+	@commiter_name
+	@commiter_email
+	@message
+	@story_id
+	@story_type
+	attr_accessor :author_name, :author_email, :commiter_name, :commiter_email, :message, :story_id, :story_type
+	#author_name, author_email, commiter_name, commiter_email and message are self explaining
+	#story_id is ID of related story or nil
+	#story_type is one of these: delivered, in_progress, nil
+
+	def initialize(author_name, author_email, commiter_name, commiter_email, message, story_id, story_type)
+		@author_name = author_name
+		@author_email = author_email
+		@commiter_name = commiter_name
+		@commiter_email = commiter_email
+		@message = message
+		@story_id = story_id
+		@story_type = story_type
+	end
+
+	def to_s
+		return "<\#Commit by =#{self.author_name} (#{self.author_email}) story_id: #{self.story_id})>"
+	end
+
+end
