f31984f64f425e0c2d4bbe7c177d18152f4de5c3
ondrej01 <ondrej.maly128@gmail.com>
2013-04-23 22:23:12 +0200

    + get_commits for testing purposes just parses local file. will return array of instances of Commit

--- script/deploy.rb
+++ script/deploy.rb
@@ -30,4 +30,25 @@ class Commit
 		return "<\#Commit by =#{self.author_name} (#{self.author_email}) story_id: #{self.story_id})>"
 	end
 
+	def get_commits
+		file = File.open("commits_examples")
+
+		commits = Array.new
+		parsed = JSON.parse(file.readline)
+		parsed.each do |act_data|
+			act_commit = Commit.new(act_data['commit']['author']['name'],
+				act_data['commit']['author']['email'],
+				act_data['commit']['committer']['name'],
+				act_data['commit']['committer']['email'],
+				act_data['commit']['message'],
+				nil,
+				nil
+			)
+			commits.push act_commit
+		end
+		return commits
+	end
+
+
+	p get_commits()
 end
