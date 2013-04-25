a386d4b0cdf88bcf42fd24e8858dc3a32ec11891
ondrej01 <ondrej.maly128@gmail.com>
2013-04-25 09:23:45 +0200

    + example file + parsing story type and id from json

--- script/deploy.rb
+++ script/deploy.rb
@@ -27,28 +27,46 @@ class Commit
 	end
 
 	def to_s
-		return "<\#Commit by =#{self.author_name} (#{self.author_email}) story_id: #{self.story_id})>"
+		return "<\#Commit by =#{self.author_name} (#{self.author_email}) story_id: #{self.story_id} story_type: #{self.story_type})>"
 	end
 
-	def get_commits
-		file = File.open("commits_examples")
-
-		commits = Array.new
-		parsed = JSON.parse(file.readline)
-		parsed.each do |act_data|
-			act_commit = Commit.new(act_data['commit']['author']['name'],
-				act_data['commit']['author']['email'],
-				act_data['commit']['committer']['name'],
-				act_data['commit']['committer']['email'],
-				act_data['commit']['message'],
-				nil,
-				nil
-			)
-			commits.push act_commit
-		end
-		return commits
-	end
-
-
-	p get_commits()
 end
+
+def get_story_from_message(message)
+	story_id = nil
+	story_type = nil
+	if message
+		message.sub(/\[.*#(.*?)\]/) { story_id = $1 }
+		message.sub(/\[(.*?)#.*\]/){ story_type = $1 }
+	end
+	ret = [story_id,story_type]
+	p ret
+	return ret
+end
+
+def get_commits
+	file = File.open("commits_examples")
+
+	commits = Array.new
+	parsed = JSON.parse(file.readline)
+	parsed.each do |act_data|
+		message = act_data['commit']['message']
+		story = get_story_from_message(message)
+		story_id = story[0]
+		story_type = story[1]
+
+		act_commit = Commit.new(act_data['commit']['author']['name'],
+			act_data['commit']['author']['email'],
+			act_data['commit']['committer']['name'],
+			act_data['commit']['committer']['email'],
+			message,
+			story_id,
+			story_type
+		)
+		commits.push act_commit
+	end
+	return commits
+end
+
+commits = get_commits()
+#p commits
